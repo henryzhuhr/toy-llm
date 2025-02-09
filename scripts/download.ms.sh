ea778a837278aeefed372d71e24c66dbc52f758c
henryzhuhr <henry.zhuhr@icloud.com>
2025-02-09 17:17:54 +0800

    更新获取模型的脚本

--- scripts/download.ms.sh
+++ scripts/download.ms.sh
@@ -1,17 +1,23 @@
-git lfs install
-
 download_from="https://www.modelscope.cn"
+model_dir="$HOME/data"
 modelid_list=(
-    Qwen/Qwen2-0.5B
-    Qwen/Qwen2-0.5B-Instruct
-    Qwen/Qwen2-1.5B
-    Qwen/Qwen2-1.5B-Instruct
-    Qwen/Qwen2-7B
-    Qwen/Qwen2-7B-Instruct
-    Qwen/Qwen2-72B
-    Qwen/Qwen2-72B-Instruct
+    Qwen/Qwen2.5-0.5B
+    Qwen/Qwen2.5-0.5B-Instruct
+    Qwen/Qwen2.5-1.5B
+    Qwen/Qwen2.5-1.5B-Instruct
+    Qwen/Qwen2.5-7B
+    Qwen/Qwen2.5-7B-Instruct
+    deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B
+    deepseek-ai/DeepSeek-R1-Distill-Qwen-7B
 )
 
+
+if [ ! -d $model_dir ]; then
+    mkdir -p $model_dir
+fi
+
+cd $model_dir
 for mi in "${modelid_list[@]}"; do
-    git clone $download_from/${mi}.git ~/data/$mi
+    git lfs install
+    git clone $download_from/${mi}.git
 done
