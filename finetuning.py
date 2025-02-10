8428ac2bec5673459bde426680d471f3d7a4f6de
henryzhuhr <henry.zhuhr@icloud.com>
2025-02-10 22:51:45 +0800

    添加一些训练的代码

--- finetuning.py
+++ finetuning.py
@@ -2,6 +2,9 @@
 
 import os
 
+import pandas as pd
+import transformers
+
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  # see issue #152
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"  # Set to the GPU ID (0 for T4)
 import argparse
@@ -16,9 +19,12 @@ from transformers import (
     TrainingArguments,
     DataCollatorForLanguageModeling,
 )
+from transformers.models.qwen2.modeling_qwen2 import Qwen2ForCausalLM
+from transformers.models.qwen2.tokenization_qwen2 import Qwen2Tokenizer
 from datasets import Dataset
 from peft import PeftModel, LoraConfig, get_peft_model, prepare_model_for_kbit_training
 from datetime import datetime
+from datasets import load_dataset
 
 
 def main():
@@ -32,8 +38,20 @@ def main():
     else:
         print("GPU not available.")
 
+    # Load the dataset
+    dataset = _load_dataset()
+
     # Load the model
     model, tokenizer = _load_model(args.model_id)
+
+    # Train the model
+    train(model, tokenizer, dataset)
+
+
+def _load_dataset(dataset_id: str = "HuggingFaceH4/helpful-instructions"):
+    dataset = load_dataset("HuggingFaceH4/helpful-instructions")
+    print(pd.DataFrame(dataset["train"]))
+    return dataset
 
 
 def _load_model(pretrained_model_name_or_path):
@@ -41,14 +59,14 @@ def _load_model(pretrained_model_name_or_path):
         load_in_4bit=True,  # Enables loading the model using 4-bit quantization, reducingmemory and computational costs.
         bnb_4bit_use_double_quant=True,
         bnb_4bit_quant_type="nf4",
-        bnb_4bit_compute_dtype=torch.bfloat16,  # Sets the computational data type for the 4-bit quantized model, controlling precision during inference or training.
+        bnb_4bit_compute_dtype=torch.float16,  # Sets the computational data type for the 4-bit quantized model, controlling precision during inference or training.
     )
 
     """
     Load the model from local or Hugging Face
     """
     # Load the model
-    model = AutoModelForCausalLM.from_pretrained(
+    model: Qwen2ForCausalLM = AutoModelForCausalLM.from_pretrained(
         pretrained_model_name_or_path,
         quantization_config=bnb_config,
         trust_remote_code=True,
@@ -57,7 +75,7 @@ def _load_model(pretrained_model_name_or_path):
     model.enable_input_require_grads()  # 开启梯度检查点时，要执行该方法
 
     # Load the tokenizer
-    tokenizer = AutoTokenizer.from_pretrained(
+    tokenizer: Qwen2Tokenizer = AutoTokenizer.from_pretrained(
         pretrained_model_name_or_path,
         use_fast=False,
         trust_remote_code=True,
@@ -78,6 +96,37 @@ def _load_model(pretrained_model_name_or_path):
     # set the pad token to indicate that it's the end-of-sentence
     tokenizer.pad_token = tokenizer.eos_token
     return model, tokenizer
+
+
+def train(model: Qwen2ForCausalLM, tokenizer: Qwen2Tokenizer, dataset):
+    OUTPUT_DIR = f"experiments-{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
+
+    # training arguments
+    training_args = transformers.TrainingArguments(
+        per_device_train_batch_size=2,
+        gradient_accumulation_steps=4,
+        num_train_epochs=10,
+        learning_rate=2e-4,
+        fp16=True,
+        save_total_limit=3,
+        logging_steps=1,
+        output_dir=OUTPUT_DIR,
+        max_steps=200,  # try more steps if you can
+        optim="paged_adamw_8bit",
+        lr_scheduler_type="cosine",
+        warmup_ratio=0.05,
+        report_to="tensorboard",
+    )
+    trainer = transformers.Trainer(
+        model=model,
+        train_dataset=dataset,
+        args=training_args,
+        data_collator=transformers.DataCollatorForLanguageModeling(
+            tokenizer, mlm=False
+        ),
+    )
+
+    pass
 
 
 class TrainDataset:
