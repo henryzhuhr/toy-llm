4d2df0dfb0d72fa200d5395309bed37569ef2b03
henryzhuhr <henry.zhuhr@icloud.com>
2025-06-05 00:52:52 +0800

    run(main): 更新模型并调整搜索工具
    - 将 OLLAMA_MODEL_NAME 环境变量的默认值从 "qwen2.5:3b" 改为 "qwen2.5:7b"
    - 注释掉 BaiduSearchTool，启用 TavilySearchResults
    - 修改示例查询内容
    - 优化输出格式

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import asyncio
 import os
 import uuid
 from datetime import datetime
-from typing import List
+from typing import List, Optional
 
 from langchain_community.tools.tavily_search import TavilySearchResults  # noqa: F401
 from langchain_core.messages import AnyMessage
@@ -17,12 +17,12 @@ from toy_agent.tools.baidu_search import BaiduSearchTool
 
 async def main():
     base_url = os.getenv("OLLAMA_BASE_URL", "http://localhost:11434")
-    model_name = os.getenv("OLLAMA_MODEL_NAME", "qwen2.5:3b")
+    model_name = os.getenv("OLLAMA_MODEL_NAME", "qwen2.5:7b")
     llm = ChatOllama(base_url=base_url, model=model_name)
 
     tools = [
-        # TavilySearchResults(max_results=3),
-        BaiduSearchTool(max_results=1),
+        TavilySearchResults(max_results=1),
+        # BaiduSearchTool(max_results=1),
     ]
     flow = PlanAndExecutorFlow(llm, tools=tools)
     app = flow.build_workflow()
@@ -44,7 +44,7 @@ async def main():
     )
 
     start_state = PlanAndExecuteAgentState(
-        input="联合国安理会最近一次会议讨论了什么议题",
+        input="深圳大学有几个校区，分别在哪",
     )
     async for event in app.astream(
         start_state,
@@ -53,14 +53,18 @@ async def main():
     ):
         # print(event)
         for k, v in event.items():
+            v: dict
             logger.info(f"🤖 [asteam:{k}] {v}")
             messages: List[AnyMessage] = v.get("messages", [])
-            last_message: AnyMessage = None
+            last_message: Optional[AnyMessage] = None
             if messages:
                 last_message = messages[-1]
                 logger.warning(f"🤖 [asteam:{k}] [{last_message.type}] {last_message}")
             # if k != "__end__":
-            #     logger.info(v)
+            #     print(f"🤖 \033[01;36m{v}\033[0m")
+            if v.get("response"):
+                print(f"\n✅ \033[01;35m{v.get('response')}\033[0m\n")
+                break
 
 
 if __name__ == "__main__":
